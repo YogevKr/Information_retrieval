c80e043f395942999d806a4f7abcfeb91c52cb7b
yogev <yogev.kr@gmail.com>
2018-06-05 22:27:31 +0300

    Fix warnings

--- src/Program.java
+++ src/Program.java
@@ -3,22 +3,14 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
-import com.sun.corba.se.impl.oa.toa.TOA;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.misc.HighFreqTerms;
-import org.apache.lucene.misc.TermStats;
 import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.search.ScoreDoc;
 
 
 public class Program {
 
-
     private static final int NUM_OF_STOP_WORDS = 20;
     private static final double ALPHA = 0.5;
     private static final double BETA = 1;
-
 
     private static File m_DocsFile;
     private static File m_QueryFile;
@@ -27,13 +19,13 @@ public class Program {
     private static String m_WorkingDir;
     private static String m_RetrievalAlgorithm = "";
     private static SearchEngine m_SearchEngine;
-    private static Map<Integer, Map> m_QueriesResults;
+    private static Map<Integer, Map<String, Float>> m_QueriesResults;
     private static Map<Integer, String[]> m_Truth;
 
 
     public static void main(String[] args) throws IOException, ParseException {
 
-        if (args.length != 1){
+        if (args.length != 1) {
             System.out.println("Software except exactly one parameter");
             System.exit(1);
         }
@@ -82,7 +74,7 @@ public class Program {
             executeAllQueries();
             F = runExperiment();
 
-            if (F > bestF){
+            if (F > bestF) {
                 bestF = F;
                 bestT = T;
             }
@@ -98,7 +90,7 @@ public class Program {
     private static double runExperiment() {
         double sumOfF = 0, sumOfP = 0, sumOfR = 0;
 
-        for (int i = 1; i <= m_Truth.size(); i++){
+        for (int i = 1; i <= m_Truth.size(); i++) {
 
             //TP
             ArrayList<String> predictionDocs = new ArrayList<>(m_QueriesResults.get(i).keySet());
@@ -116,8 +108,8 @@ public class Program {
             truth.removeAll(predictionDocs);
             int FN = truth.size();
 
-            double precision = (TP + FP != 0) ? TP/(TP + FP + 0.0) : 0 ;
-            double recall = TP/(TP + FN + 0.0);
+            double precision = (TP + FP != 0) ? TP / (TP + FP + 0.0) : 0;
+            double recall = TP / (TP + FN + 0.0);
 
             sumOfP += precision;
             sumOfR += recall;
@@ -134,13 +126,13 @@ public class Program {
         return sumOfF / m_Truth.size();
     }
 
-    private static void parseTheTruth(String i_PathToTheTruth){
+    private static void parseTheTruth(String i_PathToTheTruth) {
         ArrayList<String> lines = Utils.fileToLineList(i_PathToTheTruth);
         Map<Integer, String[]> truth = new HashMap<>();
 
-        for (String line : lines){
+        for (String line : lines) {
             if (!line.equals("")) {
-                String[] sp = line.split(" +",2);
+                String[] sp = line.split(" +", 2);
                 truth.put(Integer.parseInt(sp[0]), sp[1].split(" +"));
             }
         }
@@ -149,7 +141,7 @@ public class Program {
 
     private static void executeAllQueries() throws IOException, ParseException {
         ArrayList<String> queries = m_SearchEngine.GetQueriesFromFile(m_QueryFile);
-        Map<Integer, Map> queriesResults = new HashMap<>();
+        Map<Integer, Map<String, Float>> queriesResults = new HashMap<>();
 
         for (int i = 0; i < queries.size(); i++) {
             Map<String, Float> scoreDocs = m_SearchEngine.GetScoreDocsForQuery(queries.get(i));
@@ -164,13 +156,13 @@ public class Program {
         ArrayList<Integer> queriesIds = new ArrayList<>(m_QueriesResults.keySet());
         Collections.sort(queriesIds);
 
-        for (int id: queriesIds) {
-            ArrayList<String> docsList = new ArrayList<String>(m_QueriesResults.get(id).keySet());
+        for (int id : queriesIds) {
+            ArrayList<String> docsList = new ArrayList<>(m_QueriesResults.get(id).keySet());
             Collections.sort(docsList);
 
             StringBuilder docsId = new StringBuilder();
 
-            for (String docId: docsList){
+            for (String docId : docsList) {
                 docsId.append(docId).append(" ");
             }
 
@@ -183,28 +175,25 @@ public class Program {
     private static void initFromParameterFile(String i_parameterFilePath) {
         ArrayList<String> lines = Utils.fileToLineList(i_parameterFilePath);
 
-        for (String line: lines) {
+        for (String line : lines) {
 
-            if (line.startsWith("queryFile=")){
+            if (line.startsWith("queryFile=")) {
                 m_QueryFile = new File(line.substring(line.indexOf('=') + 1));
-            }
-            else if ((line.startsWith("docsFile="))){
+            } else if ((line.startsWith("docsFile="))) {
                 m_DocsFile = new File(line.substring(line.indexOf('=') + 1));
-            }
-            else if ((line.startsWith("outputFile="))){
+            } else if ((line.startsWith("outputFile="))) {
                 try {
                     m_OutputFile = new FileWriter(line.substring(line.indexOf('=') + 1));
                 } catch (IOException e) {
                     e.printStackTrace();
                     System.exit(1);
                 }
-            }
-            else if ((line.startsWith("retrievalAlgorithm="))){
+            } else if ((line.startsWith("retrievalAlgorithm="))) {
                 m_RetrievalAlgorithm = line.substring(line.indexOf('=') + 1);
             }
         }
 
-        if (!(m_RetrievalAlgorithm.equals("basic") || m_RetrievalAlgorithm.equals("improved"))){
+        if (!(m_RetrievalAlgorithm.equals("basic") || m_RetrievalAlgorithm.equals("improved"))) {
             System.out.println("Invalid Retrieval Algorithm!");
             System.exit(1);
         }
